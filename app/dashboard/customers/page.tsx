043a0293dd545f291a66e9cf41b7c6f0c3e7b76b
Izsal Qurlinas Afandi <izsalavandi@gmail.com>
2023-11-19 12:13:34 +0700

    Learn Next.js from chapter 1-3

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const pageCustomers = () => {
+  return (
+    <div>
+      <p>Page Customers</p>
+    </div>
+  );
+};
+
+export default pageCustomers;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const pageInvoices = () => {
+  return (
+    <div>
+      <p>Invoices Pages</p>
+    </div>
+  );
+};
+
+export default pageInvoices;
